5f8152aa583454010e991092da7eb1daad1a8d61
mborzyszkowski <maciej.borzyszkowski@gmail.com>
2020-10-07 21:04:53 +0200

    New endpoints definitions

---
+++ WarehouseSystem.Api/Query/ProductResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WarehouseSystem.Query
+{
+    public class ProductResult
+    {
+        
+        public long Id { get; set; }
+        public DateTime AddDate { get; set; }
+        public string ManufacturerName { get; set; }
+        public string ModelName { get; set; }
+        public double Price { get; set; }
+        public long Quantity { get; set; }
+    }
+}
