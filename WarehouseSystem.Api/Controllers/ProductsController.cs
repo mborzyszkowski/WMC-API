2bc1bd9f6a9181670d00811332eb3fdf011ee434
mborzyszkowski <maciej.borzyszkowski@gmail.com>
2020-10-07 21:04:53 +0200

    New endpoints definitions

--- WarehouseSystem.Api/Controllers/ProductsController.cs
+++ WarehouseSystem.Api/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -6,6 +7,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WarehouseSystem.Core.Entity;
+using WarehouseSystem.Query;
 using WarehouseSystem.Repository;
 
 namespace WarehouseSystem.Controllers
@@ -26,9 +28,34 @@ namespace WarehouseSystem.Controllers
         /// <returns>All products info in warehouse</returns>
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public async Task<ActionResult<IReadOnlyCollection<Product>>> SearchAllProducts(CancellationToken token) =>
-            await _context.Products
-                .Include(p => p.QuantityChanges)
-                .ToListAsync(token);
+        //TODO: rewrite to return quantity, not changes
+        public async Task<ActionResult<IReadOnlyCollection<ProductResult>>> SearchAllProducts(CancellationToken token) =>
+            throw new NotImplementedException();
+        
+        [HttpGet("{productId}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        //TODO: rewrite to return quantity, not changes
+        public async Task<ActionResult<ProductResult>> GetProduct(long productId, CancellationToken token) =>
+            throw new NotImplementedException();
+        
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult> AddProduct([FromBody] ProductForm productForm, CancellationToken token) =>
+            throw new NotImplementedException();
+        
+        [HttpPut("{productId}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult> UpdateProduct(long productId, [FromBody] ProductForm productForm, CancellationToken token) =>
+            throw new NotImplementedException();
+        
+        [HttpDelete]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult> DeleteProduct(long productId, CancellationToken token) => 
+            throw new NotImplementedException();
+        
+        [HttpPut("quantity/{productId}/{quantityChange}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult> ChangeQuantity(long productId, long quantityChange, CancellationToken token) => 
+            throw new NotImplementedException();
     }
 }
---
+++ WarehouseSystem.Api/Query/ProductForm.cs
@@ -0,0 +1,9 @@
+﻿namespace WarehouseSystem.Query
+{
+    public class ProductForm
+    {
+        public string ManufacturerName { get; set; }
+        public string ModelName { get; set; }
+        public double Price { get; set; }
+    }
+}
