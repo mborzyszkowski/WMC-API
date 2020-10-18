e373e63499c08cf4a619010d8144b81f4365b4c7
mborzyszkowski <maciej.borzyszkowski@gmail.com>
2020-10-18 18:14:25 +0200

    Log user operations

--- WarehouseSystem.Core/Entity/Product.cs
+++ WarehouseSystem.Core/Entity/Product.cs
@@ -7,18 +7,20 @@ namespace WarehouseSystem.Core.Entity
     {
         public long Id { get; set; }
         public DateTime AddDate { get; set; }
+        public WmcUser AddUser { get; set; }
         public string ManufacturerName { get; set; }
         public string ModelName { get; set; }
         public double Price { get; set; }
         public List<ProductQuantityChange> QuantityChanges { get; set; }
 
-        public static Product CreateNewProduct(string manufacturerName, string modelName, double price) =>
+        public static Product CreateNewProduct(string manufacturerName, string modelName, double price, WmcUser addUser) =>
             new Product
             {
                 ManufacturerName = manufacturerName,
                 ModelName = modelName,
                 Price = price,
                 AddDate = DateTime.Now,
+                AddUser = addUser,
                 QuantityChanges = new List<ProductQuantityChange>(),
             };
 
